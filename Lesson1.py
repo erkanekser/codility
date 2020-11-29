40a5ecd36f7bc6640cf9b8964ae897b28a4ec454
erkanekser <52164941+erkanekser@users.noreply.github.com>
2020-11-29 16:07:04 +0300

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,26 @@
+MAXINT = 2147483647
+def solution(N):
+    if not isinstance(N,int):
+        raise TypeError('Must be integer')
+    if N>MAXINT:
+        raise ValueError('Must be smaller')
+    if N<1:
+        raise ValueError('Must be bigger 1')
+    binary_string = str(bin(N))[2:]
+    #print(binary_string)
+    a =binary_string.split("1")
+    #print(a)
+    #print(a[-1])
+    if len(a)<=2:
+        return 0
+    if binary_string[-1]=='0':
+        a.pop(-1)
+     #   print(a)
+    max=0
+    for i in range(0,len(a)):
+    #    print(len(a[i]))
+        if max<len(a[i]):
+            max=len(a[i])
+    return max
+print(solution(32))
+print(solution(6))
